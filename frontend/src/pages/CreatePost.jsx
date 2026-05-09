3a9b6858771a20af4903d8f732fe3fa809bbd8c7
suyashbandhiwal <suyashbandhiwal1@gmail.com>
2026-05-10 03:59:20 +0530

    fix createpost url

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -10,7 +10,7 @@ function CreatePost() {
   const handleCreate = async () => {
     const token = localStorage.getItem('token')
     await axios.post(
-      'https://blog-app-backend-ndv9.onrender.com/api/auth/login',
+       'https://blog-app-backend-ndv9.onrender.com/api/posts',
       { title, content,author: 'Suyash' },
       { headers: { Authorization: token } }
     )
